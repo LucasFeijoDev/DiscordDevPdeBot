a547a8ac66b0d8184e1b990906405c7cba672942
Lucas Feijó <104742854+lukita0811@users.noreply.github.com>
2023-02-06 08:57:33 -0300

    BotHelp
    Adicionei uma forma com que o BOT consiga ajudar pessoas que apenas marcou ele, assim que ele é marcado em um chat ele volta a mensagem "Olá meu amigo, você pode digitar ".help" para obter ajuda.".

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+#Blibiotecas, comandos e libraries
+
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import os
 
+#TOKEN para acesso
+
 TOKEN = 'MTA2NTAyMzMwOTI5ODgwNjc5NA.GndGbj.6Yz39Jg6WkPA-RJNiO0TPZWL_bQmvNHJ88MWns'
 
+#Intenção, permissão e prefixo do BOT
+
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 client = discord.Client(intents=intents)
+
+#Mensagem no console quando estiver online
 
 @client.event
 async def on_ready():
     print('Estou online e funcionando!')
+
+#Assim que marcado o BOT oferece ajuda
+
+@client.event 
+async def on_message(message):
+    if client.user.mention in message.content:
+        await message.channel.send('Olá meu amigo, você pode digitar ".help" para obter ajuda.')
 
 client.run(TOKEN)    
