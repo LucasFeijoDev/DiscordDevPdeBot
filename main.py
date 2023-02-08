46a1a0744ee9dab577a44c514270247c6f589eb1
Lucas Feijó <104742854+lukita0811@users.noreply.github.com>
2023-02-08 16:27:37 -0300

    Erros
    Estava com alguns erros nos comando e vi que se eu colocar client invés de BOT isso melhora algumas coisas.

--- main.py
+++ main.py
@@ -11,43 +11,41 @@ bot = commands.Bot(command_prefix = 'p!', intents = intents)
 TOKEN = 'TOKEN AQUI'
 
 #Falar para o console que o BOT está on
-@bot.event
+@client.event
 async def on_ready():
     print('Estou online e funcionando!')
     try: 
-        synced = await bot.tree.sync()
+        synced = await client.tree.sync()
         print(f'{len(synced)} comando(s) sincronizados.')
     except Exception as e:
         print(e)
 
 
 #Quando alguém marcar o BOT dar uma mensagem
-@bot.event 
+@client.event
 async def on_message(message):
-    if bot.user.mention in message.content:
-        await message.channel.send('Olá meu amigo, você pode digitar "p!ajuda" para obter ajuda.')
-
+    if message.mentions and client.user in message.mentions:
+        await message.channel.send('Olá meu amigo, você pode digitar "p!ajuda" para ver oque eu tenho pra você.')
 
 
 #Comando de ajuda
-
-@bot.event
+@client.event
 async def on_message(message):
     if message.content == 'p!ajuda':
         await message.channel.send('Estou configurando certinho o comando de ajuda, mas em breve eu posso te ajudar.')
 
 
-##Comando de apresentação
-@bot.tree.command(name='ola')
+#Comando de apresentação
+@client.tree.command(name='ola')
 async def ola(interaction: discord.Interaction):
-    await interaction.response.send_message(f'Olá {interaction.user.mention}! Eu sou o PDEBOT.')
+    await interaction.response.send_message(f'Olá {interaction.user.mention}! eu sou o PDEBOT.')
     
 
 #Comando de falar por você
-@bot.tree.command(name='falar')
+@client.tree.command(name='falar')
 @app_commands.describe(palavra_frase = 'Oque eu deveria falar por você?')
 async def say(interaction: discord.Interaction, palavra_frase: str):
     await interaction.response.send_message(f'{interaction.user.name} falou: {palavra_frase}')
 
 
-bot.run(TOKEN)    
+client.run(TOKEN)
