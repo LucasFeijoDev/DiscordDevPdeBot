fa4a7db196aa8f1c002fda5d2f4229b12f8f722e
LucasFeijoDev <LucasFeijoDev@gmail.com>
2023-07-23 01:30:08 -0300

    Finalizando as ultimas finalizações do .env e do gitignore e arrumando os codigos

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import discord
-import os 
 from discord.ext import commands
 from discord import app_commands
+
+import os
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix = 'p!', intents = intents)
+
+load_dotenv()
+print(os.getenv('TOKEN'))
 
 TOKEN = os.getenv('TOKEN')
 
@@ -35,37 +40,5 @@ else:
     async def on_message(message):
         if message.content == 'p!ajuda':
             await message.channel.send('Estou configurando certinho o comando de ajuda, mas em breve eu posso te ajudar.')
-
-
-    #Comando de apresentação
-    @bot.tree.command(name='ola')
-    async def ola(interaction: discord.Interaction):
-        await interaction.response.send_message(f'Olá {interaction.user.mention}! Eu sou o PDEBOT.')
-        
-
-    #Comando de falar por você
-    @bot.tree.command(name='falar')
-    @app_commands.describe(palavra_frase = 'Oque eu deveria falar por você?')
-    async def say(interaction: discord.Interaction, palavra_frase: str):
-        await interaction.response.send_message(f'{interaction.user.name} falou: {palavra_frase}')
-
-
-    #Comando para kickar/banir
-    @bot.command()
-    @commands.has_permissions(kick_members=True)
-    async def kick(ctx, member:discord.Member, *, reason=None):
-        if reason == None:
-            reason='Sem motivo'
-        await ctx.guild.kick(member)
-        await ctx.send(f'Usuário {member.mention} foi kickado do servidor por {reason}')
-
-
-    @bot.command()
-    @commands.has_permissions(ban_members=True)
-    async def ban(ctx, member:discord.Member, *, reason=None):
-        if reason == None:
-            reason='Sem motivo'
-        await ctx.guild.ban(member)
-        await ctx.send(f'Usuário {member.mention} foi banido do servidor por {reason}')
 
     bot.run(TOKEN)    
