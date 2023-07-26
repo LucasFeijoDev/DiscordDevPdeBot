28f2f93859cf6686c908c636293248baa356e246
LucasFeijoDev <LucasFeijoDev@gmail.com>
2023-07-26 13:14:15 -0300

    Arrumando o erro de sempre mandar o mesmo commit

--- main.py
+++ main.py
@@ -72,9 +72,13 @@ else:
 
         while not bot.is_closed():
             last_commit = check_for_commits()
-            await channel.send(f'Novo commit no repositório {GITHUB_REPO}! SHA: {last_commit}')
-            await asyncio.sleep(60)  # Verifica a cada 60 segundos
 
+            if last_commit and last_commit != last_commit_sent:
+                await channel.send(f'Novo commit no repositório {GITHUB_REPO}!')
+                last_commit_sent = last_commit
+
+            await asyncio.sleep(60)  # Verifica a cada 60 segundos
+   
     # Falar para o console que o BOT está on
     @bot.event
     async def on_ready():
